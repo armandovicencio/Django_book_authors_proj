aa7764aaee6941af74526284127562bb696bc62c
Armando Vicencio <armand.vicencio@gmail.com>
2022-02-21 17:25:52 -0300

    Se incorporarn Templates

--- books_authors_app/urls.py
+++ books_authors_app/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
+
+
+
 urlpatterns = [
-    path('', views.index),
-    
+    path('add_book', views.add_book, name="add_book"),
+    path('book_detail/<int:book_id>', views.book_detail, name="book_detail"),
+    path('', views.all_books, name="books"),
+
+    path('add_author', views.add_author, name="add_author"),
+    path('authors/<int:author_id>', views.author_detail, name="author_detail"),
+    path('authors', views.all_authors, name="authors"),
+    path('add_book_to_author', views.add_book_to_author, name="add_book_to_author")
 ]
